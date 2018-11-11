37940435f38888eee55e6445ba656c1b2800a205
notebook <notebook@192.168.1.5>
2018-11-11 13:37:05 -0200

    [...]

--- src/main/java/modelo/tabuleiro/TipoSolo.java
+++ src/main/java/modelo/tabuleiro/TipoSolo.java
@@ -3,17 +3,17 @@ package modelo.tabuleiro;
 public enum TipoSolo {
 	
 	
-	GRAMA("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/grass_green2y_d.jpg"),
+	GRAMA("/grass_green2y_d.jpg"),
 	
-	AGUA("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/grass_autumn_n.jpg"),
+	AGUA("/grass_autumn_n.jpg"),
 		
-	PONTE("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/desert_mntn_s.jpg"),
+	PONTE("/desert_mntn_s.jpg"),
 		
-	MONTANHA("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/mntn_canyon_d.jpg"),
+	MONTANHA("/mntn_canyon_d.jpg"),
 	
-	BASE_1("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/ground_ferns_d.jpg"),
+	BASE_1("/ground_ferns_d.jpg"),
 	
-	BASE_2("/home/leopoldo/Jean/Code/Java/draaaaaaaaaaaaaalAgain/src/main/snow_grass3_d.jpg");
+	BASE_2("/snow_grass3_d.jpg");
 	
 	private String imagePath;
 	
